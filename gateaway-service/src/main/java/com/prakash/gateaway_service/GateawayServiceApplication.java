28acde1e2cfb692c32ca100b3e79880dfc26bc85
Prakash Chakraborty <Prakash.Chakraborty1@outlook.com>
2026-04-27 17:10:50 -0500

    first commit

---
+++ gateaway-service/src/main/java/com/prakash/gateaway_service/GateawayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prakash.gateaway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateawayServiceApplication.class, args);
+	}
+
+}
